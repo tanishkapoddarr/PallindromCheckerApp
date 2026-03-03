1d1a4565f0958c06a4a167ef8bb1abd57ba1115c
tanishkapoddarr <tp1002@srmist.edu.in>
2026-03-03 09:38:06 +0530

    [tanishkapoddarr]UC5: Character Array Based Palindrome Check

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,2 +1,21 @@
+import java.util.Stack;
 public class PallindromeCheckerApp {
+    public static boolean isPalindrome(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+        for( char c : str.toCharArray()){
+            if(stack.pop()!=c){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String str = "noon";
+        boolean result = isPalindrome(str);
+        System.out.println("Input: " +str);
+        System.out.println("Is Palindrome?: " +result);
+    }
 }
