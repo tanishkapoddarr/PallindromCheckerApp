a6a25531de8a9b6019e7663360817a5ba088dfad
Satwikjajula <satwikjajula@gmail.com>
2026-03-03 09:11:29 +0530

    [tanishkapoddarr]UC4: Character Array Based Palindrome Check

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,2 +1,21 @@
 public class PallindromeCheckerApp {
+    public static boolean isPalindrome(String str) {
+        char[] chars = str.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String str = "radar";
+        boolean result = isPalindrome(str);
+        System.out.println("Input: " +str);
+        System.out.println("Is Palindrome?: " +result);
+    }
 }
